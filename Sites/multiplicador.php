fde78295014ae94de24c70c14345b463d7b8dbcf
F-arellano <pipe.arellano98@gmail.com>
2020-04-08 19:24:56 -0400

    modulo php

--- Sites/multiplicador.php
+++ Sites/multiplicador.php
@@ -3,5 +3,5 @@ $valor1=$_POST["valor 1"];
 $valor2=$_POST["valor 2"];
 $resultado=$valor1*$valor2;
 
-echo "<h4>valor retornado: $resultado</h4>";
+echo "<h4>el producto de $valor1 y $valor2 es: $resultado</h4>";
 ?>
