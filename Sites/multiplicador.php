dd53802535061b0d8bb160aecf8bc2aad6cb2d5b
F-arellano <pipe.arellano98@gmail.com>
2020-04-08 19:29:28 -0400

    correcciones multiplicador.php

--- Sites/multiplicador.php
+++ Sites/multiplicador.php
@@ -1,7 +1,7 @@
 <?php
 $valor1=$_POST["valor_1"];
 $valor2=$_POST["valor_2"];
-$resultado="nada";
+$resultado=$valor1 * $valor2;
 
 echo "<h4>el producto de $valor1 y $valor2 es: $resultado</h4>";
 ?>
