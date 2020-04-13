b8a033939d7c6ed565a66b0b10af398365aec10b
F-arellano <pipe.arellano98@gmail.com>
2020-04-13 16:57:35 -0400

    correccion db a la que se enlazaba PDO

--- Sites/configuracion/conexion_db.php
+++ Sites/configuracion/conexion_db.php
@@ -2,7 +2,7 @@
   try {
     $user = 'grupo105';
     $password = 'grupo105';
-    $databaseName = 'grupo105';
+    $databaseName = 'grupo105e2';
     $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e, F";
