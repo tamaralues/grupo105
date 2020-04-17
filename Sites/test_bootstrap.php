bf40b881f414af13962fac7d0798b13e3b45c180
F-arellano <pipe.arellano98@gmail.com>
2020-04-16 21:15:04 -0400

    css

--- Sites/test_bootstrap.php
+++ Sites/test_bootstrap.php
@@ -53,17 +53,12 @@
   <div class="card-deck mb-3 text-center">
     <div class="card mb-4 shadow-sm">
       <div class="card-header">
-        <h4 class="my-0 font-weight-normal">Consultar <i>username</i></h4>
+        <h4 class="my-0 font-weight-normal">Consultar <i>username</i><br>y correo electrónico</h4>
       </div>
       <div class="card-body">
         <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>10 users included</li>
-          <li>2 GB of storage</li>
-          <li>Email support</li>
-          <li>Help center access</li>
-        </ul>
-        <button type="button" class="btn btn-lg btn-block btn-primary">Sign up for free</button>
+
+        <button type="button" class="btn btn-lg btn-block btn-primary">Consultar</button>
       </div>
     </div>
     <div class="card mb-4 shadow-sm">
