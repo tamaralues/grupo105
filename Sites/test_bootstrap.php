2ad84f8576f515599428edce8d459d10a1bdfa16
F-arellano <pipe.arellano98@gmail.com>
2020-04-17 17:43:27 -0400

    css

--- Sites/test_bootstrap.php
+++ Sites/test_bootstrap.php
@@ -61,7 +61,7 @@
         <ul class="list-unstyled mt-3 mb-4">
           <li><h6 class="my-0 font-weight-normal"> retorna una lista con todos los <i>username</i> y su correo asociado</h6></li>
         </ul>
-        <form class="form-group d-flex flex-column" action="consultas/username_correo.php" method="post">
+        <form action="consultas/username_correo.php" method="post">
           <button type="submit" class="btn btn-lg btn-block btn-primary mt-auto form-control">Consultar</button>
         </form> 
       </div>
