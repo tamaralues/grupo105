1ce3d946a6edd1fd4b5304491b4feb653ac738c4
F-arellano <pipe.arellano98@gmail.com>
2020-04-17 13:31:45 -0400

    css

--- Sites/test_bootstrap.php
+++ Sites/test_bootstrap.php
@@ -59,9 +59,11 @@
         <h4 class="my-0 font-weight-normal"> probando texto</h4>
         <h3 class="my-0 font-weight-normal"> probando texto</h3>
         <br>
-
-
-        <button type="button" class="btn btn-lg btn-block btn-primary">Consultar</button>
+        <form align="center" action="consultas/ciudades_del_pais.php" method="post"> 
+         <input type="text" name="pais">
+         <input type="submit" value="Ver consulta">
+          <button type="button" class="btn btn-lg btn-block btn-primary">Consultar</button>
+        </form>
       </div>
     </div>
     <div class="card mb-4 shadow-sm">
@@ -87,8 +89,6 @@
         <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
         <ul class="list-unstyled mt-3 mb-4">
           <li>30 users included</li>
-          <li><h2>15 GB of storage</h2></li>
-          <li><h1>Phone and email support</h1></li>
         </ul>
         <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
       </div>
