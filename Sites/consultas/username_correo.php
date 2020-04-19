2005a603eef0d402007b32adadf7fbaecde398f9
F-arellano <pipe.arellano98@gmail.com>
2020-04-19 19:36:56 -0400

    css query

--- Sites/consultas/username_correo.php
+++ Sites/consultas/username_correo.php
@@ -43,21 +43,36 @@
     <!-- Custom styles for this template -->
     <link href="pricing.css" rel="stylesheet">
   </head>
-  <body>
-  <div>
-    <table class="table">
-      <thead class="table">
-        <tr>
-          <th>username</th>
-          <th>correo</th>
-        </tr>
-        <tr>
-          <td>pelpi</td>
-          <td>01</td>
-        </tr>
-      </thread>
-    </table>
-  <div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
     <?php
     #crea el PDO para realizar las consultas
     require("../configuracion/conexion_db.php");
