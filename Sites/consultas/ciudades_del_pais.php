a9d52462ce491c21249d11a08cbb53a0d516f9cb
F-arellano <pipe.arellano98@gmail.com>
2020-04-19 19:12:57 -0400

    template query

--- Sites/consultas/ciudades_del_pais.php
+++ Sites/consultas/ciudades_del_pais.php
@@ -1,3 +1,7 @@
+<!-- Bootstrap core CSS -->
+<link href="css/main.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
 <body>
 <?php
 #crea el PDO para realizar las consultas
