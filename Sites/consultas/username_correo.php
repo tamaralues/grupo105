a3a1ac1dedc11cacc5353dc1391d64d26d973e75
F-arellano <pipe.arellano98@gmail.com>
2020-04-19 19:41:54 -0400

    css query

--- Sites/consultas/username_correo.php
+++ Sites/consultas/username_correo.php
@@ -11,7 +11,7 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/pricing/">
 
     <!-- Bootstrap core CSS -->
-<link href="css/main.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="../css/main.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <!-- Favicons -->
 <link rel="apple-touch-icon" href="icon/apple-touch-icon.png" sizes="180x180">
