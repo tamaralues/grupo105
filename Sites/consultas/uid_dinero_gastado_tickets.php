4f5ea70a1d33188511dd28b9a720cd0c266a3ac1
F-arellano <pipe.arellano98@gmail.com>
2020-04-20 00:32:15 -0400

    query 4

--- Sites/consultas/uid_dinero_gastado_tickets.php
+++ Sites/consultas/uid_dinero_gastado_tickets.php
@@ -51,7 +51,7 @@ require("../configuracion/conexion_db.php");
 $id = (int)$_POST["uid"];
 #se realiza la consulta, esta no tiene inputs
 $query = "SELECT  uid, tid, precio FROM tickets_comprados natural join datos_viaje natural join usuarios where uid=$id;";
-$query_total = "SELECT SUM(PRECIO) FROM tickets_comprados natural join datos_viaje natural join usuarios where uid=$id;";
+$query_total = "SELECT SUM(precio) FROM tickets_comprados natural join datos_viaje natural join usuarios where uid=$id;";
 
 #se asocia la consulta a una db, se ejecuta y el resultado se guarda en una variable
 $result = $db -> prepare($query);
@@ -76,8 +76,8 @@ $consulta_total = $result_total -> fetchAll();
         foreach ($consulta as $p) {
           echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td></tr>";
       }
-        foreach ($consulta as $p) {
-            echo "<tr><td>Total:</td><td></td><td>$consulta_total[0]</td></tr>";
+        foreach ($consulta_total as $p) {
+            echo "<tr><td>Total:</td><td></td><td>$p[0]</td></tr>";
         }
       ?>
   </table>
