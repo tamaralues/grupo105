03c0bcb8b8cda50f7d062d3ff49732ca4669684d
F-arellano <pipe.arellano98@gmail.com>
2020-04-20 01:07:49 -0400

    query 6

--- Sites/consultas/dinero_gastado_entre_fechas.php
+++ Sites/consultas/dinero_gastado_entre_fechas.php
@@ -51,8 +51,7 @@ require("../configuracion/conexion_db.php");
 $fecha_inicio= $_POST["fecha_inicial"];
 $fecha_termino =$_POST["fecha_final"];
 #se realiza la consulta, esta no tiene inputs
-$query = "SELECT uid, username, precio FROM usuarios natural join tickets_comprados natural join datos_viaje
-     WHERE fechainicio >= $fecha_inicio and fechatermino <= $fechatermino;";
+$query = "SELECT uid, username, precio FROM usuarios natural join tickets_comprados natural join datos_viaje;";
 
 #se asocia la consulta a una db, se ejecuta y el resultado se guarda en una variable
 $result = $db -> prepare($query);
