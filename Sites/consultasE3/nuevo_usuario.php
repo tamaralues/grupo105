7b8af9d0d787659cfb96de2ec7856d39faf72a28
F-arellano <pipe.arellano98@gmail.com>
2020-05-12 11:28:01 -0400

    intento registro con function

--- Sites/consultasE3/nuevo_usuario.php
+++ Sites/consultasE3/nuevo_usuario.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+?>
 <head>
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,7 +17,7 @@
                 <h2 class="text-center">Regístrate</h2>
                 <img class="img-fluid mx-auto d-block rounded" style="width: 35%;"
                     src="https://www.simplyhealth.co.uk/shcore/sh/furniture/images/svgs/top-nav-account-icon.svg" />
-                <form action="confirmacion_registro.php" method="post">
+                <form action="nuevo_usuario.php" method="post">
                     <div class="form-group">
                         <label for="user"><b>Nombre de usuario</b></label>
                         <input id="user" name="user"
@@ -46,7 +47,7 @@
                             class="form-control" type="password"
                             placeholder="password">
                     </div>
-                    <button type="submit" class="btn btn-dark btn-block mb-2">
+                    <button type="submit" class="btn btn-dark btn-block mb-2" name="button_registro" value="button_registro">
                         Registrarme
                     </button>
                 </form>
@@ -57,4 +58,60 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="../css/bootstrap.js"></script>
 </body>
+
+<?php #funcion registro usuario
+function registrar(){
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../configuracion/conexion_db_e3.php");
+
+  #Se obtiene el valor del input del usuario
+  $name = $_POST["name"];
+  $direccion = $_POST["direccion"];
+  $user = $_POST["user"];
+  $correo = $_POST["correo"];
+  $pwd = $_POST["pwd"];
+
+ #Consulta para saber si existe el usuario/correo, y cual UID correspondería
+ $query_uid = "SELECT uid, username, correo, password, nombreusuario, direccionusuario FROM usuarios NATURAL JOIN cuentas order by uid desc;";
+
+ #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+   $result_uid = $db -> prepare($query_uid);
+   $result_uid -> execute();
+   $fetch_uid = $result_uid -> fetchAll();
+
+   $last_uid = 0;
+   $bool_correo = FALSE;
+   $bool_username = FALSE;
+   foreach ($fetch_uid as $p) {
+     if ($last_uid < $p[0]){
+       $last_uid=$p[0];
+     }
+     if ($p[1] == $user){
+         $bool_username = TRUE;
+     }
+     if ($p[2] == $correo) {
+         $bool_correo = TRUE;
+     }
+ }
+ #id del usuario que se debe añadir
+ $last_uid +=1;
+if (!$bool_username && !$bool_correo) {
+   // echo "ejecutando inserciones";
+  #Se construye la consulta como un string
+    $query_usuario = "INSERT INTO usuarios(uid, username, correo, password) VALUES ('$last_uid', '$user', '$correo', '$pwd');";
+    $query_cuentas = "INSERT INTO cuentas(nombreusuario, username, direccionusuario) VALUES ('$name', '$user', '$direccion');";
+
+  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+	$result_usuario = $db -> prepare($query_usuario);
+    $bool_usuario = $result_usuario -> execute();
+    
+    $result_cuentas = $db -> prepare($query_cuentas);
+    $bool_cuentas = $result_cuentas -> execute();
+    }
+
+    if(isset($_POST['button_registro']))
+{
+   registrar();
+} 
+}
 </html>
