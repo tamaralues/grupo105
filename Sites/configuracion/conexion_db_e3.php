a707d0ff605ed6810ec1f72dd2640bac2fa770db
Tamara <tylues@uc.cl>
2020-05-13 02:37:01 -0400

    m

--- Sites/configuracion/conexion_db_e3.php
+++ Sites/configuracion/conexion_db_e3.php
@@ -8,13 +8,5 @@
     echo "No se pudo conectar a la base de datos: $e, F";
   }
 
-  try {
-    $user = 'grupo102';
-    $password = 'grupo102';
-    $databaseName = 'grupo102e3';
-    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
-  } catch (Exception $e) {
-    echo "No se pudo conectar a la base de datos: $e, F";
-  }
-  
+
 ?>
