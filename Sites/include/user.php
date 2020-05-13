f510ee6bb880e47884ff16b3bdf259bf23459574
F-arellano <pipe.arellano98@gmail.com>
2020-05-13 12:54:20 -0400

    fix bugs

--- Sites/include/user.php
+++ Sites/include/user.php
@@ -10,13 +10,14 @@ class User {
     }
 
     public function userExists($user, $pass) {
-        $query = "SELECT uid FROM usuarios WHERE username=$user AND password=$pass";
+        $query = "SELECT uid, username FROM usuarios WHERE username=$user AND password=$pass";
         $result = $this->db -> prepare($query);
         $result ->execute();
         $fetch = $result -> fetchAll();
         $number = 0;
         foreach ($fetch as $f){
             $number+=1;
+            echo "<p>$f[1]</p>";
         }
         if ($result){
             echo "<p>query exitosa</p>";
