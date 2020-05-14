e64b9683b4f6df3c1ad680bf32d7f7f3021482b7
Tamara <tylues@uc.cl>
2020-05-14 03:17:48 -0400

    avances cosulta reservas

--- Sites/consultasE3/consultas_reservas.php
+++ Sites/consultasE3/consultas_reservas.php
@@ -17,18 +17,16 @@
 
 <div class="card mb-4 shadow-sm">
   <div class="card-header">
-    <h4 class="my-0 font-weight-normal">Consulta 3</h4>
+    <h4 class="my-0 font-weight-normal">Reaslizar reserva</h4>
     </div>
   <div class="card-body">
-    <h3 class="card-title pricing-card-title">Paises visitados por Usuario</h3>
     <ul class="list-unstyled mt-3 mb-4">
-    <li><h6 class="my-0 font-weight-normal"> retorna una lista con todos los países en que se ha hospedado el usuario ingresado</h6></li>
     </ul>
     <form align="center" action="consultas/username_hospedajes.php" method="post">
-      <input type="date" class="form-control" name="fechainicio" aria-describedby="emailHelp" placeholder="Ingrese la fecha de ingreso (yyyy-mm-dd)">
-      <input type="date" class="form-control" name="fechatermino" aria-describedby="emailHelp" placeholder="ingrese la fecha de salida (yyyy-mm-dd)">
+      <input type="date" class="form-control" name="fechainicio" aria-describedby="emailHelp" placeholder="Ingrese la fecha de ingreso">
+      <input type="date" class="form-control" name="fechatermino" aria-describedby="emailHelp" placeholder="ingrese la fecha de salida">
       <br>
-      <button type="submit" class="btn btn-lg btn-block btn-primary">Consultar</button>
+      <button type="submit" class="btn btn-lg btn-block btn-primary">Reservar</button>
     </form>
   </div>
 </div>
