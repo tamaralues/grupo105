4758ba331951532ce45d50df27b65c9078f7fa76
Tamara <tylues@uc.cl>
2020-05-15 00:03:42 -0400

    arreglo ubicación header

--- Sites/consultasE3/confirmacion_reserva.php
+++ Sites/consultasE3/confirmacion_reserva.php
@@ -1,5 +1,5 @@
 
 
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
-      <?php include_once "includes/header.php"; ?>
+      <?php include_once "../header.php"; ?>
 </div>
