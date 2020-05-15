359240d350d8c980d13be74ab535f759fdc7af77
Tamara <tylues@uc.cl>
2020-05-14 23:50:02 -0400

    prueba nav bar

--- Sites/consultasE3/confirmacion_reserva.php
+++ Sites/consultasE3/confirmacion_reserva.php
@@ -1,8 +1,21 @@
 
 
-<?php include '../header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrapE3.css" rel="stylesheet">
+    <link href="css/estiloE3.css" rel="stylesheet">
+    <title>Documento random</title>
+</head>
 
-<div class="card mb-4 shadow-sm">
-<nav class="my-2 my-md-0 mr-md-3">
-</div>
+
+<!-- Navbar definition -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <nav>
+          <a href="header.php" class="my-2 my-md-0 mr-md-3">NebStore</a>
+        </nav>
+      </div>
+    </div>
