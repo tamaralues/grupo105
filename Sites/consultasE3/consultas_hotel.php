8b4c4d1b01d1c4352fd6feb422f5e2ac56e4b0e7
Tamara <tylues@uc.cl>
2020-05-15 12:35:36 -0400

    arreglar reservas prueba

--- Sites/consultasE3/consultas_hotel.php
+++ Sites/consultasE3/consultas_hotel.php
@@ -18,11 +18,10 @@
 			}
 		}
 
-    $query_comentarios = "SELECT uid, comentario FROM Comentarios WHERE hid = '$hid' ";
+    $query_comentarios = "SELECT uid FROM reservas; ";
     $result_cm = $db -> prepare($query_comentarios );
     $result_cm -> execute();
     $comentarios_escritos = $result_cm -> fetchAll();
-
 
 ?>
 
