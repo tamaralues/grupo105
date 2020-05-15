bed18af75d7474499a53596a686c372455709220
Tamara <tylues@uc.cl>
2020-05-14 23:22:04 -0400

    prueba nav bar

--- Sites/header.php
+++ Sites/header.php
@@ -161,28 +161,6 @@ $query_drop1 = "SELECT nombrepais, pid FROM paises;";
         </div>
     </div>
 
-<?php
-#testeo inicio de sesion
-if (isset($_SESSION['user'])){
-    echo "<p>hay sesion iniciada</p>";
-    $user->setUser($user_session->getCurrentUser());
-} else if (isset($_POST['username']) && isset($_POST['pwd'])){
-    $user_form = $_POST['username'];
-    $pwd_form = $_POST['pwd'];
-    echo "<p>validando login: username - $user_form, pwd - $pwd_form</p>";
-    if ($user->userExists($user_form, $pwd_form)) {
-        echo "<p>usuario validado</p>";
-        $user_session -> setCurrentUser($user_form);
-        $user -> setUser($user_form);
-    } else {
-        $error_login = "nombre, correo o pwd incorrecto";
-        echo "<p>algo salio mal</p>";
-    }
-} else {
-    echo "<p>hay que logear</p>";
-}
-?>
-
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
