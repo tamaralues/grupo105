0126353a90961244e72d9f4f07ffbc1acc470f0b
Tamara <tylues@uc.cl>
2020-05-14 22:51:09 -0400

    reservas

--- Sites/consultasE3/confirmacion_reserva.php
+++ Sites/consultasE3/confirmacion_reserva.php
@@ -1,8 +1,9 @@
 <?php
 		session_start();
-		echo '
-		<div class="container body-content" style="margin-top:50px">
-			<h2>Bienvenido '.$_SESSION['user'].'</h2>
-			<p class="lead"></p>
-		</div>'
-	?>
+		echo
+?>
+
+<div class="container body-content" style="margin-top:50px">
+	<h2>Bienvenido '.$_SESSION['user'].'</h2>
+	<p class="lead"></p>
+</div>'
