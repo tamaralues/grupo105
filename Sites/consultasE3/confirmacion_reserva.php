bb10e2762b744d9b5bac305b6ee32f430ef580dd
Tamara <tylues@uc.cl>
2020-05-15 01:30:32 -0400

    llamada del hid

--- Sites/consultasE3/confirmacion_reserva.php
+++ Sites/consultasE3/confirmacion_reserva.php
@@ -4,7 +4,7 @@
     #Se obtiene el valor del input del usuario
     $hotel = $_POST["hotel"];
 
-		$query = "SELECT hid FROM hoteles where nombrehotel = $hotel;";
+		$query = "SELECT hid FROM hoteles where nombrehotel ~* $hotel;";
 
 		$result = $db -> prepare($query);
     $result -> execute();
