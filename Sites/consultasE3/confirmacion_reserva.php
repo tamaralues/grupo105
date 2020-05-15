4f3388c23100a43b0e9823f9aca023a6dd7ed371
Tamara <tylues@uc.cl>
2020-05-14 23:24:59 -0400

    prueba nav bar

--- Sites/consultasE3/confirmacion_reserva.php
+++ Sites/consultasE3/confirmacion_reserva.php
@@ -1,12 +1,2 @@
 <?php include '../header.php';
 ?>
-
-
-<?php
-		session_start();
-		echo '
-		<div class="container body-content" style="margin-top:50px">
-			<h2>Bienvenido '.$_SESSION['user'].'</h2>
-			<p class="lead"></p>
-		</div>'
-?>
