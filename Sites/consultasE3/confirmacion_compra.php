dbe2d9229269dabb195aed8d39328db345e94201
Tamara <tylues@uc.cl>
2020-05-17 00:23:00 -0400

    Agregué query

--- Sites/consultasE3/confirmacion_compra.php
+++ Sites/consultasE3/confirmacion_compra.php
@@ -7,6 +7,13 @@
     $destino = $_POST["destino"];
     $horasalida = $_POST["horasalida"];
     $medio =  $_POST["medio"];
+    $fechaviaje = $_POST["fechaviaje"];
+
+    $user = $_SESSION['username'];
+    $uid = $_SESSION['id'];
+
+    $fechacompra =  getdate();
+
 
     $query_drop4 = "SELECT did, cid_origen, capacidad FROM datos_viaje  where cid_destino = '$destino'   ,cid_origen = '$origen', horasalida = '$horasalida', medio = '$medio' ;";
 
@@ -16,6 +23,7 @@
      $fetch_drop4 = $result_drop4 -> fetchAll();
 
      $count = 0;
+     $did = 0
      $capacidad_ocupada = 0;
 
      foreach ($fetch_drop4 as $f4) {
@@ -23,17 +31,43 @@
      }
 
      foreach ($fetch_drop4 as $f4) {
-       if($fetch_drop4[1] = $origen && $capacidad_ocupada != $capacidad){
+       if($f4[1] = $origen && $capacidad_ocupada != $capacidad){
          $count = 1;
+         $did = $f4[0];
        }else{
          $count = 0;
        }
      }
 
+     $query_tick = "SELECT tid FROM tickets_comprados;";
+     #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+       $result_tick = $db -> prepare($query_tick);
+       $result_tick -> execute();
+       $fetch_tick = $result_tick -> fetchAll();
+
+       $last_cmid = 0;
+
+       foreach ($fetch_tick as $p) {
+         if ($last_tick < $p[0]){
+           $last_tick = $p[0];
+         }
+     }
+
+     $last_tick += 1;
+
+
+
      if($count = 1){
        echo "<p>La compra fue realizada con exito </p>";
+       $query_add = "INSERT INTO tickets_comprados VALUES ('$last_tick ', '$did', '$uid', '$fechacompra', '$fechaviaje ');";
+       $result_add = $db  -> prepare($query_add);
+       $result_add -> execute();
      }else{
        echo "<p>lo siento no se pudo realizar la compra</p>";
      }
 
+
 ?>
+
+<?php echo "<p>$capacidad_ocupada</p>"; ?>
+<?php echo "<p>$capacidad</p>"; ?>
