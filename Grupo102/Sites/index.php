a5b112003721520613557f99eff4f700b03a8ec8
Diego Mendieta <damendieta@uc.cl>
2020-05-22 15:34:11 -0400

    Agrego archivos de Sites del grupo 102

---
+++ Grupo102/Sites/index.php
@@ -0,0 +1,21 @@
+<?php include('templates/header.html');   ?>
+
+<body>
+  <h1 align="center">Entrega 3 </h1>
+  <p style="text-align:center;">Aquí podrás encontrar información sobre la E3.</p>
+
+  <br>
+
+  <h3 align="center"> </h3>
+
+  <form align="center" action="consultas/artistas.php" method="post">
+    <br/>
+    <br/>
+    <input type="submit" value="Artistas">
+  </form>
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</html>
