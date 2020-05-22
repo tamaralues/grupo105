d5f7cc7f4578d3236bc5ccadd60b43b1cf3e882b
F-arellano <pipe.arellano98@gmail.com>
2020-05-22 15:25:35 -0400

    css consultas

--- Sites/consultasE3/comprar_tickets.php
+++ Sites/consultasE3/comprar_tickets.php
@@ -33,8 +33,8 @@
 <head>
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="css/bootstrapE3.css" rel="stylesheet">
-    <link href="css/estiloE3.css" rel="stylesheet">
+    <link href="../css/bootstrapE3.css" rel="stylesheet">
+    <link href="../css/estiloE3.css" rel="stylesheet">
     <title>Documento random</title>
 </head>
 
