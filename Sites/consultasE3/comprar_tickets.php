d0ccd761788fbb433870d11c55aa7a91b26e7f94
F-arellano <pipe.arellano98@gmail.com>
2020-05-22 16:05:43 -0400

    css

--- Sites/consultasE3/comprar_tickets.php
+++ Sites/consultasE3/comprar_tickets.php
@@ -47,8 +47,8 @@ $user -> setUser($post_username);
 <head>
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="css/bootstrapE3.css" rel="stylesheet">
-    <link href="css/estiloE3.css" rel="stylesheet">
+    <link href="../css/bootstrapE3.css" rel="stylesheet">
+    <link href="../css/estiloE3.css" rel="stylesheet">
     <title>Compra de tickets</title>
 </head>
 
@@ -140,6 +140,6 @@ $user -> setUser($post_username);
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-      <script src="css/bootstrap.js"></script>
+      <script src="../css/bootstrap.js"></script>
 </body>
 </html>
