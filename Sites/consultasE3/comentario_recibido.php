a358e578e7709166989343037c1c887fafcd1dac
Tamara <tylues@uc.cl>
2020-05-22 19:38:44 -0400

    prueba volver

--- Sites/consultasE3/comentario_recibido.php
+++ Sites/consultasE3/comentario_recibido.php
@@ -50,7 +50,15 @@
     ?>
 
   <div>Comentario fue recibido con éxito</div>
-  <a <p href="consultas_hotel.php">Volver atrás</a></p>
+  <form action ="consultas_hotel.php" method="POST">
+
+        <input type="hidden" name="hotel" value= "<?php echo $hid ;?>"  >
+        <br>
+        <button type="submit" class="btn btn-dark btn-block mb-2">
+            Volver
+        </button>
+        </form>
+
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
