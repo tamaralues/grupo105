aa7d04469b38ea872a129be48b56ad4c69c88540
Tamara <tylues@uc.cl>
2020-05-22 21:13:53 -0400

    borre prints

--- Sites/consultasE3/realizacion_reserva.php
+++ Sites/consultasE3/realizacion_reserva.php
@@ -79,13 +79,13 @@
 
 
 
-<?php echo "<p>$user</p>"; ?>
-<?php echo "<p>$uid</p>"; ?>
-<?php echo "<p>$last_rid</p>"; ?>
-<?php echo "<p>$hotel</p>"; ?>
-<?php echo "<p>$hid </p>"; ?>
-<?php echo "<p>$fechainicio</p>"; ?>
-<?php echo "<p>$fechatermino</p>"; ?>
+<?php #echo "<p>$user</p>"; ?>
+<?php #echo "<p>$uid</p>"; ?>
+<?php #echo "<p>$last_rid</p>"; ?>
+<?php #echo "<p>$hotel</p>"; ?>
+<?php #echo "<p>$hid </p>"; ?>
+<?php #echo "<p>$fechainicio</p>"; ?>
+<?php #echo "<p>$fechatermino</p>"; ?>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
