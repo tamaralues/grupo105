a3bbb695420ba0062e0235b9e9d4132058fbda5a
Tamara <tylues@uc.cl>
2020-05-24 01:37:09 -0400

    avances front reservas

--- Sites/consultasE3/comprar_tickets.php
+++ Sites/consultasE3/comprar_tickets.php
@@ -287,7 +287,7 @@ $user -> setUser($post_username);
                 }
                 ?>
         </form>
-    </div>
+
 
     <form action ="comprar_tickets.php" method="POST">
           <br>
@@ -308,6 +308,8 @@ $user -> setUser($post_username);
               Volver
           </button>
     </form>
+
+    </div>
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
