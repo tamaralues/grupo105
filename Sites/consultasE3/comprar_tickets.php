0381a7f3220caf9c75488963576d52767dfce468
Tamara <tylues@uc.cl>
2020-05-23 21:28:24 -0400

    problema medio

--- Sites/consultasE3/comprar_tickets.php
+++ Sites/consultasE3/comprar_tickets.php
@@ -221,6 +221,7 @@ $user -> setUser($post_username);
                 </form>
             </div>
     </div>
+    <?php echo "<p>$medio medio </p>"; ?>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
     <h3 class="display-4">Tickets vendidos</h3>
