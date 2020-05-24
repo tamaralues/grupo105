734c86bbf5e7e51777478530e0d0ee0676e821a2
F-arellano <pipe.arellano98@gmail.com>
2020-05-24 13:49:12 -0400

    ds

--- Sites/consultasE3/comprar_tickets.php
+++ Sites/consultasE3/comprar_tickets.php
@@ -116,7 +116,7 @@ else{
 
        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
 
-       <h3 class="display-4">Comprar Tickets, usuario:<?php echo "$user->$username" ?></h3>
+       <h3 class="display-4">Comprar Tickets, usuario:<?php echo "" ?></h3>
        </div>
 
           <form action = <?php echo $direccion ;?> method="POST">
