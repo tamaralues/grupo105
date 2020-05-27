6552b70b7a697e5152965949994d7e9f2e750fe5
Tamara <tylues@uc.cl>
2020-05-27 13:01:41 -0400

    arreglo direccionamiento

--- Sites/consultasE3/artistase3.php
+++ Sites/consultasE3/artistase3.php
@@ -19,7 +19,7 @@
 
 	<?php foreach ($artista as $a): ?>
   		<tr> <td><?php echo "$a[1]"?></td> <td>
-         <form  align="left" action="artista_especifico.php" method="post">
+         <form  align="left" action="artistas_especificoe3.php" method="post">
     <button type="submit" name="artista" value= <?php echo "$a[0]"?> class="btn-link">Ir a Artista</button>
         </form>
 </td> </tr>
