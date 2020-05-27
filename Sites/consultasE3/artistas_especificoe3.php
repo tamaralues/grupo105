8a6f567b9b22b691b547c8b451d7346dc168dd82
Tamara <tylues@uc.cl>
2020-05-27 13:12:36 -0400

    front artistas especificos

--- Sites/consultasE3/artistas_especificoe3.php
+++ Sites/consultasE3/artistas_especificoe3.php
@@ -2,17 +2,13 @@
 <html lang="en">
 
 <head>
+
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="../css/bootstrapE3.css" rel="stylesheet">
-    <link href="../css/style_usuarioE3.css" rel="stylesheet">
-    <title>iniciar sesion</title>
+    <link href="../css/estiloE3.css" rel="stylesheet">
+    <title>Compra de tickets</title>
 </head>
-
-
-
-
-
 
 <body>
 
@@ -151,3 +147,10 @@ foreach ($artista as $a) {
     <br/>
     <input type="submit" value="Artistas">
   </form>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="../css/bootstrap.js"></script>
+</body>
+</html>
