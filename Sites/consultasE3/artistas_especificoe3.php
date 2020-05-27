1c321ed3a162b73ed769f015f736b9eaae928ba8
Tamara <tylues@uc.cl>
2020-05-27 12:52:48 -0400

    arreglo de obras en navbar

--- Sites/consultasE3/artistas_especificoe3.php
+++ Sites/consultasE3/artistas_especificoe3.php
@@ -1,7 +1,7 @@
 <body>
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+  require("../configuracion/conexion.php");
 
   #Se obtiene el valor del input del usuario
   $idartista = (int)($_POST["artista"]);
