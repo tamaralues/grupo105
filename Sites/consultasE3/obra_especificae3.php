bb335e3a3c64722099d309848c07d522f080454d
Tamara <tylues@uc.cl>
2020-05-28 16:40:44 -0400

    fotos obras

--- Sites/consultasE3/obra_especificae3.php
+++ Sites/consultasE3/obra_especificae3.php
@@ -29,7 +29,6 @@
   $artista_prev = (int)($_POST["artista"]);
 
 
-
 	#Hacer consulta 2
  	$query = "SELECT DISTINCT obras.idobra, obras.nombreobra, obras.anoinicio, obras.anotermino, obras.periodo, obras.idlugar, lugares.nombrelugar, ciudades.nombreciudad, paises.nombrepais from artistas, obrasartistas, obras,
    lugares, ciudades, paises  where  artistas.idartista = obrasartistas.idartista and obrasartistas.idobra = obras.idobra and obras.idlugar = lugares.idlugar and lugares.idciudad = ciudades.idciudad and ciudades.idpais = paises.idpais and obras.idobra = $idobra;";
@@ -56,76 +55,76 @@
     $obra_imagen = "https://historiascortas.org/wp-content/uploads/2019/08/A-Apolo-siguiendo-a-Dafne-Historias-Cortas.jpg";
   }
   elseif($idobra == 2){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/e/ea/Gerard_David_004.jpg";
   }
   elseif($idobra == 3){
-    $obra_imagen = "https://3minutosdearte.com/wp-content/uploads/2018/06/Caravaggio-Cabeza-de-Medusa-1597-e1536759112183.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Martirio_di_San_Pietro_September_2015-1a.jpg/897px-Martirio_di_San_Pietro_September_2015-1a.jpg";
   }
   elseif($idobra == 4){
-    $obra_imagen = "https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydGlzdFwvaW1hZ2VGaWxlXC9iaWxkLW90dGF2aW9fbGVvbmlfY2FyYXZhZ2dpby5qcGciLCJyZXNpemVDcm9wLDQwMCw0MDAsQ1JPUF9FTlRST1BZIl19.1jyWicmcmhjD2HUyZ71ftRHCfe8b7Y/uWLJ-vx_w-Qvk.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/%27David%27_by_Michelangelo_Fir_JBU004.jpg/540px-%27David%27_by_Michelangelo_Fir_JBU004.jpg";
   }
   elseif($idobra == 5){
-    $obra_imagen = "https://3minutosdearte.com/wp-content/uploads/2018/06/Caravaggio-Cabeza-de-Medusa-1597-e1536759112183.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Last_Judgement_%28Michelangelo%29.jpg/600px-Last_Judgement_%28Michelangelo%29.jpg";
   }
   elseif($idobra == 6){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Sandro_Botticelli_-_La_Primavera_-_Google_Art_Project.jpg/1920px-Sandro_Botticelli_-_La_Primavera_-_Google_Art_Project.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/%27Moses%27_by_Michelangelo_JBU160.jpg/500px-%27Moses%27_by_Michelangelo_JBU160.jpg";
   }
   elseif($idobra == 7){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Fede_Galizia_-_Portrait_of_Federico_Zuccari_-_WGA8432.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Michelangelo_Caravaggio_052.jpg/600px-Michelangelo_Caravaggio_052.jpg";
   }
   elseif($idobra == 8){
-    $obra_imagen = "";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Last_Judgement_%28Michelangelo%29.jpg/600px-Last_Judgement_%28Michelangelo%29.jpg";
   }
   elseif($idobra == 9){
-    $obra_imagen = "https://1.bp.blogspot.com/--KIKtapkO3w/WhmboHFDGqI/AAAAAAABBWQ/-ipO8dWN0e4t2bsjne6ft7t6uISXgZvDQCLcBGAs/s1600/01.jpeg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg";
   }
   elseif($idobra== 10){
-    $obra_imagen = "https://www.biografiasyvidas.com/biografia/b/fotos/bernini.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/5_Estancia_del_Sello_%28La_Disputa_del_Sacramento%29.jpg/222px-5_Estancia_del_Sello_%28La_Disputa_del_Sacramento%29.jpg";
   }
   elseif($idobra == 11){
     $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/8/89/La_Anunciaci%C3%B3n%2C_de_Fra_Angelico.jpg";
   }
   elseif($idobra == 12){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d1/15th-century_unknown_painters_-_Five_Famous_Men_%28detail%29_-_WGA23920.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/8/89/La_Anunciaci%C3%B3n%2C_de_Fra_Angelico.jpg";
   }
   elseif($idobra == 13){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/c/c6/David_Self_Portrait.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Piazza_San_Pietro2010_01.jpg/900px-Piazza_San_Pietro2010_01.jpg";
   }
   elseif($idobra == 14){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Duquesnoy1776.jpg/220px-Duquesnoy1776.jpg";
+    $obra_imagen = "https://sobreitalia.com/wp-content/uploads/2009/08/fuente-de-neptuno-de-ammanati.jpg";
   }
   elseif($idobra == 15){
-    $obra_imagen = "";
+    $obra_imagen = "https://es.wikiarquitectura.com/wp-content/uploads/2017/01/Fuente-de-los-cuatro-rios-Rome.jpg";
   }
   elseif($idobra == 16){
-    $obra_imagen = "https://canalhistoria.es/wp-content/uploads/2018/10/da_vinci_thumb.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/600px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg";
   }
   elseif($idobra == 17){
-    $obra_imagen = "https://mymodernmet.com/wp/wp-content/uploads/2019/04/michelangelo-facts-7.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Leonardo_da_Vinci_-_Mona_Lisa_%28Louvre%2C_Paris%29.jpg/490px-Leonardo_da_Vinci_-_Mona_Lisa_%28Louvre%2C_Paris%29.jpg";
   }
   elseif($idobra == 18){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Raffaello_Sanzio.jpg/1200px-Raffaello_Sanzio.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Michelangelo%27s_Pieta_5450_cropncleaned.jpg/490px-Michelangelo%27s_Pieta_5450_cropncleaned.jpg";
   }
   elseif($idobra == 19){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/8/89/La_Anunciaci%C3%B3n%2C_de_Fra_Angelico.jpg";
   }
   elseif($idobra == 20){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/8/89/La_Anunciaci%C3%B3n%2C_de_Fra_Angelico.jpg";
   }
   elseif($idobra == 21){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://3minutosdearte.com/wp-content/uploads/2018/06/Caravaggio-Cabeza-de-Medusa-1597-e1536759112183.jpg";
   }
   elseif($idobra == 22){
     $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
   }
   elseif($idobra == 23){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Michelangelo_-_Creation_of_Adam_%28cropped%29.jpg/600px-Michelangelo_-_Creation_of_Adam_%28cropped%29.jpg";
   }
   elseif($idobra == 24){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Sandro_Botticelli_-_La_Primavera_-_Google_Art_Project.jpg/1920px-Sandro_Botticelli_-_La_Primavera_-_Google_Art_Project.jpg";
   }
   elseif($idobra == 25){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Transfiguration_Raphael.jpg/600px-Transfiguration_Raphael.jpg";
   }
   elseif($idobra == 26){
     $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
@@ -137,7 +136,7 @@
     $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
   }
   elseif($idobra == 29){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/e/e3/Giotto_di_Bondone_090.jpg";
   }
   elseif($idobra == 30){
     $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg";
@@ -152,7 +151,7 @@
     $obra_imagen = "";
   }
   elseif($idobra == 34){
-    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/f/f2/Jos%C3%A9_de_Ribera_024.jpg";
+    $obra_imagen = "https://upload.wikimedia.org/wikipedia/commons/8/88/Polittico_di_Badia.jpg";
   }
   elseif($idobra == 35){
     // No encontré
