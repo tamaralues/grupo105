9de14176b86007c70eb282ee9ec1cfc973e273a2
Tamara <tylues@uc.cl>
2020-05-28 21:18:34 -0400

    front itinerario

--- Sites/consultasE3/itinerario.php
+++ Sites/consultasE3/itinerario.php
@@ -35,6 +35,9 @@ else{
     include_once '../nav_bar.php';
   ?>
 
+
+<div class="container px-4 py-2" style="width:90%; margin-top: 100px;">
+
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../configuracion/conexion.php");
@@ -170,3 +173,10 @@ else{
 	}
   ?>
 	</table>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="../css/bootstrap.js"></script>
+  </body>
+  </html>
