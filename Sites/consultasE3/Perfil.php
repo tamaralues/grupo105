9f5623ff3a7fcd7d93642ed84b959265a2878329
F-arellano <pipe.arellano98@gmail.com>
2020-05-29 20:23:23 -0400

    sd

--- Sites/consultasE3/Perfil.php
+++ Sites/consultasE3/Perfil.php
@@ -144,7 +144,7 @@ foreach($fetch_dinero_tickets as $f){
                                         $h=$fd[0];
                                     }
 
-                                    echo "<tr><td>$f[0]</td><td>$f[1]</td><td>$f[2]</td><td>$g</td><td>$h</td></tr>";
+                                    echo "<tr><td>$f[0]</td><td>$f[1]</td><td>$f[2]</td><td>$f[3]</td><td>$h</td></tr>";
                                 }
                                 ?>
                                 </tbody>
